6bef726b87ba22119cf3a77edbc315cf8c008b6d
mikesobczak <msobczak@yahoo.com>
2022-04-15 09:31:52 -0400

    internal solution

--- force-app/main/default/lwc/payment/payment.js
+++ force-app/main/default/lwc/payment/payment.js
@@ -1,10 +1,10 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 const ANOTHER_AMOUNT_OPTION = 'anotherAmount';
 const AMOUNT_DUE_TODAY_OPTION = 'amountDueToday';
 
 export default class Payment extends LightningElement {
-
+    @track errorStr;
     labels = {
         toast: {
             processingErrorTitle: 'Processing Error'
@@ -33,31 +33,35 @@ export default class Payment extends LightningElement {
 
         this.amountToPaySelection = event.detail.value;
 
-        if(this.amountToPaySelection !== ANOTHER_AMOUNT_OPTION) {
-            this.anotherAmount = undefined;
-
-            let anotherAmountCmp = this.template.querySelector(".anotherAmount");
-            anotherAmountCmp.setCustomValidity("");
-            anotherAmountCmp.reportValidity();
-            anotherAmountCmp.checkValidity();
-        }
+        this.isAnotherAmountRequired();
     }
 
     handleAnotherAmountChange(event) {
         const value = event.detail.value;
         this.anotherAmount = value;
+        console.log(this.anotherAmount);
     }
 
     handleAnotherAmountFocus(event) {
         this.amountToPaySelection = ANOTHER_AMOUNT_OPTION;
+        this.isAnotherAmountRequired();
     }
 
-    get isAnotherAmountRequired() {
+    isAnotherAmountRequired() {
         console.log('isAnotherAmountRequired()', this.amountToPaySelection);
-        if(this.amountToPaySelection === ANOTHER_AMOUNT_OPTION) {
+        if(this.amountToPaySelection === ANOTHER_AMOUNT_OPTION || (this.amountToPaySelection !== ANOTHER_AMOUNT_OPTION && this.errorStr)) {
             console.log('isAnotherAmountRequired() = true');
-            return true;
-        }
+            let oldErrorStr = this.errorStr;
+            this.errorStr = (!this.anotherAmount) ? 'Complete this field.' : '';
+            if (this.errorStr !== oldErrorStr){
+                console.log('theresadiff');
+                let anotherAmountCmp = this.template.querySelector(".anotherAmount");
+                anotherAmountCmp.setCustomValidity(this.errorStr);
+                anotherAmountCmp.reportValidity();
+                anotherAmountCmp.checkValidity();
+                console.log(anotherAmountCmp.checkValidity());
+            }
+        } 
         console.log('isAnotherAmountRequired() = false');
 
         return false;
